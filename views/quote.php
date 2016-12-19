a56a645b83749988cd4e2482ca0d5f709b93b5b3
SanjayBabu-sopho <sanjay.ch222@gmail.com>
2016-12-19 14:10:59 +0000

    done with views of quote

---
+++ views/quote.php
@@ -0,0 +1,6 @@
+<div>
+    <?php
+        $form_price = number_format($price, 3, '.', '');
+        print("{$form_price}");
+    ?>
+</div>
---
+++ views/quote_form.php
@@ -0,0 +1,13 @@
+<form action="/../public/quote.php" method="post">
+    <fieldset>
+        <div class="form-group">
+            <input autocomplete="off" autofocus class="form-control" name="symbol" placeholder="Enter symbol" type="text"/>
+        </div>
+        <div class="form-group">
+            <button class="btn btn-default" type="submit">
+                <span aria-hidden="true" class="glyphicon glyphicon-log-in"></span>
+                Search symbol
+            </button>
+        </div>
+    </fieldset>
+</form>
