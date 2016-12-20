e2919e23cad713cc1c2cf61bf348b8ab06d79987
SanjayBabu-sopho <sanjay.ch222@gmail.com>
2016-12-20 13:17:09 +0000

    coding of sell is ready

---
+++ public/buy.php
@@ -0,0 +1,42 @@
+<?php
+    // configuration
+    require("../includes/config.php"); 
+    
+    if($_SERVER["REQUEST_METHOD"] == "GET")
+    {
+        render("buy_form.php");
+    }
+    else
+    {
+        $check_int = ctype_digit($_POST["shares"]);
+        if ($check_int == false)
+        {
+            apologize("shares must be a postive integer");
+        }
+        $cash =CS50::query("SELECT cash from users WHERE id = ?",$_SESSION["id"]);
+        $stock = lookup($_POST["symbol"]);
+        if ($stock != false)
+        {
+            $price = $stock["price"];
+        }
+        else
+        {
+            apologize("invalid symbol");
+        }
+        $value =$_POST["shares"] * $price;
+        if ( $value > $cash[0]["cash"])
+        {
+            apologize("cash is not enough");
+        }
+        else
+        {
+            CS50::query("INSERT INTO Portfolio (user_id,symbol,shares)
+                VALUES (?,?,?) ON DUPLICATE KEY UPDATE shares = shares + ?",
+                $_SESSION["id"],$_POST["symbol"],$_POST["shares"],$_POST["shares"]);
+            CS50::query("UPDATE users SET cash = cash - ? WHERE id = ?",
+                $value,$_SESSION["id"]);
+            redirect("index.php");
+        }
+    }
+?>
+    
