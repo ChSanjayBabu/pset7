776e546278d51c6c0d6b04a732e1493e1314da34
SanjayBabu-sopho <sanjay.ch222@gmail.com>
2016-12-22 05:21:00 +0000

    extra feature is completed

---
+++ public/password.php
@@ -0,0 +1,43 @@
+<?php
+    // configuration
+    require("../includes/config.php");
+    
+    if(empty($_POST["password"]))
+    {
+        render("pass_form.php");
+    }
+    
+    
+    // else if user reached page via POST (as by submitting a form via POST)
+    else if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        $data = CS50::query("SELECT hash FROM users where id = ?",$_SESSION["id"]);
+        // checks whether user entered a password
+        if (empty($_POST["password"]))
+        {
+            apologize("You must enter your password");
+        }
+        else if(password_verify($_POST["password"], $data[0]["hash"]) == false)
+        {
+            apologize("You must enter correct present password");
+        }
+        else if (empty($_POST["new_pass"]))
+        {
+            apologize("You must provide your  new password.");
+        }
+        else if (empty($_POST["confirmation"]))
+        {
+            apologize("You must type password again for confirmation.");
+        }
+        else if (strcmp($_POST["new_pass"],$_POST["confirmation"]) != 0)
+        {
+            apologize("you must retype correct password.");
+        }
+        else
+        {
+            CS50::query("UPDATE users SET hash = ? WHERE id = ?",
+                password_hash($_POST["new_pass"], PASSWORD_DEFAULT),$_SESSION["id"]);
+            render("pass_change.php");
+        }
+    }
+?>
---
+++ views/pass_change.php
@@ -0,0 +1,10 @@
+<div>
+    <h3>Your password is changed successfully!</h3>
+</div>
+<div class="container">
+    <div id="top">
+        <ul class="nav nav-pills">
+            <li><a href="index.php"><strong>Home Page</strong></a></li>
+        </ul>
+    </div>
+</div>
